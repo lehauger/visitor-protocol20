f640ed40bceaf4b628478f466cc962570124f4b9
Lars Hauger <lehauger@gmail.com>
2026-05-13 19:31:24 +0200

    fix: rename middleware.ts to proxy.ts per Next.js 16 convention

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { auth } from '@/auth'
 
-export default auth((req) => {
+export const proxy = auth((req) => {
   if (!req.auth) {
     return Response.redirect(new URL('/login', req.url))
   }
